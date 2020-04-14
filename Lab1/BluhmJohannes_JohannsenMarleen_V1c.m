1d0ccd6e98cc1f275ab7c332b9c5eee9d3938ef0
MasterPuffin <johannes.bluhm@outlook.com>
2020-04-14 17:08:22 +0200

    Create BluhmJohannes_JohannsenMarleen_V1c.m

---
+++ Lab1/BluhmJohannes_JohannsenMarleen_V1c.m
@@ -0,0 +1,32 @@
+%Johannes Bluhm und Marleen Johannsen, 13.04.2020, Laboraufgabe 1, Version 1
+%1. Matrizenverarbeitung: Verkleinern des Bildes cameraman.tif
+
+clc; %Kommandofenster bereinigen
+clear; %Variablen bereinigen
+close all; %alles schließen
+
+img = imread('cameraman.tif'); %Bild einlesen und speichern
+
+img = double(img)/255; %Matrix in double konvertieren
+imSmall = img(50:50+35, 100:100+32); %Ausschnitt aus Bild wählen
+imBig = imSmall(floor(1:(1/4):32+(1-(1/4))), floor(1:(1/4):32+(1-(1/4)))); %Ausschnitt vergrößern
+
+figure(1); %Ein neues Fenster öffnen
+imshow(imBig); %Anzeigen des eingelesenen Bildes
+axis on; %Achsen sichtbar machen
+title('Vergrößerter Ausschnitt'); %Titel des Fensters
+xlabel('Pixel'); %Horizontale Achse Beschriftung
+ylabel('Pixel'); %Vertikale Achse Beschriftung
+
+imBig = filter([0.2 0.2 0.2 0.2 0.2], 1, imBig); %Bild filtern
+imBig = imrotate(imBig, 90); %Bild drehen
+imBig = filter([0.2 0.2 0.2 0.2 0.2], 1, imBig); %Bild filtern
+imBig = imrotate(imBig, 270); %Bild zurückdrehen
+
+figure(2); %Ein neues Fenster öffnen
+imshow(imBig); %Anzeigen des eingelesenen Bildes
+axis on; %Achsen sichtbar machen
+title('Gefilterter Ausschnitt'); %Titel des Fensters
+xlabel('Pixel'); %Horizontale Achse Beschriftung
+ylabel('Pixel'); %Vertikale Achse Beschriftung
+
