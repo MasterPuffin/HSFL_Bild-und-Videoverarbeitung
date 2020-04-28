2f0d0c6934db5f2322728ae1e8ad43ecdafd9f8e
Johannes <johannes.bluhm@outlook.com>
2020-04-28 17:00:22 +0200

    Aufgabe 2c hinzugefügt

--- Lab 2/BluhmJohannes_JohannsenMarleen_V2c.m
+++ Lab 2/BluhmJohannes_JohannsenMarleen_V2c.m
@@ -10,38 +10,34 @@ img = imread('WhiteHouse.jpg'); %Bild einlesen und speichern
 figure(1); %Ein neues Fenster öffnen
 imshow(img); %Anzeigen des eingelesenen Bildes
 axis on; %Achsen sichtbar machen
-title('cameraman.tif Originalbild'); %Titel des Fensters
+title('WhiteHouse.jpg Originalbild'); %Titel des Fensters
 xlabel('Pixel'); %Horizontale Achse Beschriftung
 ylabel('Pixel'); %Vertikale Achse Beschriftung
 
-img = imresize(img, [150,200]);
+img = imresize(img, [150,200]); %Bildgröße ändern
 
 figure(2); %Ein neues Fenster öffnen
 imshow(img); %Anzeigen des eingelesenen Bildes
 axis on; %Achsen sichtbar machen
-title('cameraman.tif Originalbild'); %Titel des Fensters
+title('WhiteHouse.jpg verkleinert'); %Titel des Fensters
 xlabel('Pixel'); %Horizontale Achse Beschriftung
 ylabel('Pixel'); %Vertikale Achse Beschriftung
 
- images    = cell(3,1);
- images{1} = imread('WhiteHouse.jpg');
- images{2} = imread('WhiteHouse.jpg');
- images{3} = imread('WhiteHouse.jpg');
+writerObj = VideoWriter('whitehouse.avi'); %Ausgabedatei festlegen
+writerObj.FrameRate = 1; %Framerate festlegen
 
- writerObj = VideoWriter('myVideo.avi');
- writerObj.FrameRate = 1;
+frames = 10; %Anzahl der Frames festlegen
+pixelCount = 0; %Initialisiere Pixelzähler
 
-secsPerImage = [5 10 15];
-
- % open the video writer
- open(writerObj);
- % write the frames to the video
- for u=1:length(images)
-     % convert the image to a frame
-     frame = im2frame(images{u});
-     for v=1:secsPerImage(u) 
-         writeVideo(writerObj, frame);
-     end
- end
- % close the writer object
- close(writerObj);
+open(writerObj); %Video writer öffnen
+%Durch alle Frames loopen
+for u=1:frames     
+    RGB = insertShape(img,'FilledRectangle',[u*10 u*10 10 10], 'color','red'); %Rechteck in Frame einfügen
+    pixelCount = numel(RGB) + pixelCount; %Pixelzähler um aktuelles Bild erhöhen
+     
+    frame = im2frame(RGB); 
+    writeVideo(writerObj, frame); %Frame ins Video schreiben
+end
+close(writerObj); %Video writer schließen
+implay('whitehouse.avi'); %Video abspielen
+disp("Pixel in Video: "+pixelCount); %Pixelcount ausgeben
