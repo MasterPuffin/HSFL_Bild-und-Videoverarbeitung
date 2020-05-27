d3b6e369fb78d99430c223b0bb2656ea1ea4b7e0
Marleen Johannsen <marleen.johannsen@g,x.de>
2020-05-27 09:55:41 +0200

    Update BluhmJohannes_JohannsenMarleen_V5.m

--- Lab 5/BluhmJohannes_JohannsenMarleen_V5.m
+++ Lab 5/BluhmJohannes_JohannsenMarleen_V5.m
@@ -36,7 +36,7 @@ s = regionprops(imgFill, 'all');
 count = size(s) %Länge des Arrays bestimmen 
 
 sm = struct([]); %Neues leeres Structure Array
-a = zeros;b = zeros;c = zeros;
+a = zeros;b = zeros;c = zeros; %Speicher reservieren
 counter = 1; %Counter für Zeilen
 
 for i=1:count(1) %(forschleife durchlaufen)
@@ -45,14 +45,11 @@ for i=1:count(1) %(forschleife durchlaufen)
         sm(counter).Area = s(i).Area; %Wert für Area kopieren
         a(counter) = s(i).Area; %Fläche für plot in einen Vektor schreiben
         
-        sm(counter).ConvexArea = s(i).ConvexArea; %Wert für ConvexArea kopieren
-        b(counter) = s(i).ConvexArea; %Konvexe Fläche für plot in einen Vektor schreiben
+        sm(counter).Eccentricity = s(i).Eccentricity; %Wert für ConvexArea kopieren
+        b(counter) = s(i).Eccentricity; %Konvexe Fläche für plot in einen Vektor schreiben
         
         sm(counter).Solidity = s(i).Solidity; %Wert für Solidität kopieren
         c(counter) = s(i).Solidity; %Solidität für plot in einen Vektor schreiben
-        
-        sm(counter).Perimeter = s(i).Perimeter; %Wert für Umfang kopieren
-        sm(counter).Orientation = s(i).Orientation; %Wert für Ausrichtung kopieren
         
         counter= counter+1; %Counter erhöhen
     end
@@ -67,8 +64,8 @@ disp(sprintf('Perimeter: %f', s(ind).Perimeter)); %Umfang
 disp(sprintf('Orientation: %f', s(ind).Orientation)); %Orientierung
 
  
-figure(2); plot(a, b, 'ro'); xlabel('Area'); ylabel('ConvexArea'); %2D Graph mit Area und ConvexArea
-figure(3); scatter3(a,b,c); xlabel('Area'); ylabel('ConvexArea'); zlabel('Solidity'); %3D Graph mit allen Features
+figure(2); plot(a, b, 'ro'); xlabel('Area'); ylabel('Eccentricity'); %2D Graph mit Area und ConvexArea
+figure(3); scatter3(a,b,c); xlabel('Area'); ylabel('Eccentricity'); zlabel('Solidity'); %3D Graph mit allen Features
 
 %Größtes Objekt
 figure(4); imshow(s(ind).Image,'InitialMagnification','fit'); %Größtes Objekt anzeigen
