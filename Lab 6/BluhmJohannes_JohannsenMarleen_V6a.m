e60aace9829c170d363506f10e2e82d05599bef5
Johannes <johannes.bluhm@outlook.com>
2020-06-01 18:08:30 +0200

    added templates

---
+++ Lab 6/BluhmJohannes_JohannsenMarleen_V6a.m
@@ -0,0 +1,6 @@
+%Johannes Bluhm und Marleen Johannsen, 03.06.2020, Laboraufgabe 6, Version 1
+%Objekterkennung Vorverarbeitung
+
+clc; %Kommandofenster bereinigen
+clear; %Variablen bereinigen
+close all; %alles schlieﬂen
