37924d4a0acafc8ad786a62b1f0157276354a494
Marleen Johannsen <marleen.johannsen@g,x.de>
2020-06-03 11:42:00 +0200

    Corrections from Fuchs 3.6.

--- Lab 6/BluhmJohannes_JohannsenMarleen_V6b.m
+++ Lab 6/BluhmJohannes_JohannsenMarleen_V6b.m
@@ -1,4 +1,4 @@
-%Johannes Bluhm und Marleen Johannsen, 03.06.2020, Laboraufgabe 6, Version 1
+%Johannes Bluhm und Marleen Johannsen, 03.06.2020, Laboraufgabe 6, Version2
 %Objekterkennung Nummernschilder
 
 clc; %Kommandofenster bereinigen
@@ -18,16 +18,14 @@ disp("Bildgröße:"); disp(size(img)); %Größe ausgeben
 imgGr = rgb2gray(img); %In Graustufen umwandeln
 thresh = graythresh(img); %Automatisches Thresholding
 imgBW = imbinarize(imgGr, thresh); %Threshold
-
 figure(2); %Neues Fenster
 imshow(imgBW, 'InitialMagnification','fit'); %Anzeigen SW Bild
 title("Schwarz-weiß Bild"); %Titel
 
 %% F Ausschneiden
 imgInv = ~imgBW; %Invertieren
-imLabel = bwlabel(imgInv, 8); %Labeln
-imgF = zeros(size(imgInv)); %Neues Bild
-imgF = or (imgF, (imLabel==2)); %Verknüpfen mit Region 2 (=F)
+imgInv = imfill(imgInv, 'holes');
+imgF = imgInv(3:end, 1:floor(end/2));% nur linke Seite (F) verwenden
 figure(3); %neues fenster
 imshow(imgF, 'InitialMagnification','fit'); %Anzeigen des F
 title("Ausgeschnittenes F");%titel
@@ -38,23 +36,24 @@ N_abtast = 32;
 N_FD = 80; %Anzahl FD
 
 Poly=funcGenPolyPic(imgF, N_abtast);%Polygonzug berechnen
-refPoly=FuncInvarianteFourierDeskriptoren(1, 1, Poly, N_FD); %referenzpolygon FD berechnen
+refPoly=FuncInvarianteFourierDeskriptoren(1, 1, Poly, N_FD); %Referenzpolygon FD berechnen
 
 figure(4);%Fenster öffnen
 plot(real(Poly),imag(Poly), 'b*-');%Referenzpolygonzug zeigen
 title("Polygonzug F"); %Titel
 
 figure(5); %Fenster öffnen
-bar(abs(refPoly));%Spektrum anzeigen
+bar(refPoly);%Spektrum anzeigen
 title("Spektrum F");%titel
 
 %% Vorverarbeites Bild laden
-imgP =imbinarize(imread('Parkplatz_vorverarbeitet.jpg')); %Bild einlesen und speichern und in SW umwandeln
-figure(6); imshow(imgP, 'InitialMagnification','fit'); hold on; %Bild anzeigen für später und Fenster offen halten
-
+imgPre =imbinarize(imread('Parkplatz_vorverarbeitet.jpg')); %Bild einlesen und speichern und in SW umwandeln
+imgOrg =imread('Parkplatz.jpg'); %Bild einlesen und speichern und in SW umwandeln
+figure(6); imshow(imgOrg, 'InitialMagnification','fit'); hold on; %Bild anzeigen für später und Fenster offen halten
+title("Erkannte F's im Originalbild");
 %% F erkennen
 
-s = regionprops(imgP, 'image', 'centroid'); %regionprops bestimmen
+s = regionprops(imgPre, 'image', 'centroid'); %regionprops bestimmen
 count = size(s); %Größe für for-Schleife
 
 for i=1:count(1) %in forSchleife jedes Bild und Schwerpunkt speichern
@@ -63,15 +62,15 @@ for i=1:count(1) %in forSchleife jedes Bild und Schwerpunkt speichern
     polygon = funcGenPolyPic(im, N_abtast); %von jedem Bild den Polygonzug bestimmen
 
     FDVec = FuncInvarianteFourierDeskriptoren(1, 1, polygon, N_FD); %...und die Fourierdiskriptoren berechnen
-    abstand(i) = sum( abs(refPoly - FDVec) ); %abstand zueinander berechnen
+    abstand(i) = sum(abs(refPoly - FDVec) ); %abstand zueinander berechnen
     % erkannte Objekte ins Originalbild eintragen
-    if ( abstand(i) < 0.5 ) %wenn Abstand kleiner 0.5 dann...
+    if ( abstand(i) < 0.3) %wenn Abstand kleiner 0.3 dann...
         figure(6); plot(centroid(1), centroid(2), 'r*'); %F erkannt und Schwerpunkt einzeichnen
-        disp("KoordinateX"); sprintf('%f',centroid(1))
-        disp("KoordinateY"); sprintf('%f',centroid(2))
-        figure(7); %neues Fenster
-        bar(abs(abstand)); %Abstand zeigen
-        title("Abstand der Fourier-Diskriptoren der entsprechenden Objekte");
+        disp("KoordinateX"); sprintf('%f',centroid(1)) %X-Koordinate
+        disp("KoordinateY"); sprintf('%f',centroid(2)) %Y-Koordinate
     end
 end
 
+  figure(7); %neues Fenster
+  bar(abstand); %Abstand zeigen
+  title("Abstand der Fourier-Diskriptoren der entsprechenden Objekte");%Titel
